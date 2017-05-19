6fb44573e939a56a8238aa6dd2ecfa3e5703fac8
Anthony Guimard <anthony.guimard@savoirfairelinux.com>
2017-05-19 15:50:56 -0400

    Fix router

--- app/App.jsx
+++ app/App.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router,
+  Switch,
   Route
 } from 'react-router-dom';
 
 import './styles/main.scss';
 
 import Home from './pages/Home.jsx';
+import NotFound from './pages/NotFound.jsx';
 
 ReactDOM.render(
   <Router>
-    <div>
-      <Route exact path='*' component={Home} />
-    </div>
+    <Switch>
+      <Route path='/toto' component={NotFound} />
+      <Route path='/' component={Home} />
+    </Switch>
   </Router>,
   document.getElementById('react-root')
 );
--- app/pages/Home.jsx
+++ app/pages/Home.jsx
@@ -5,6 +5,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className="landing">
+        <h1 className="landing__title"> React electron ring client POC</h1>
         <div className="App-inner">
           <Logo logoPath='../public/images/logo-ring-beta2-tagline-EN-couleur.svg' />
           {/* <Logo logoPath='../public/images/logo-ring.svg' /> */}
