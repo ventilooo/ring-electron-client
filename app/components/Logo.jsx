fed64917cbfbcf3859d1980909c374a9bb947cf5
Anthony Guimard <anthony.guimard@savoirfairelinux.com>
2017-05-19 16:06:59 -0400

    Add demo effect.

--- app/components/Logo.jsx
+++ app/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Logo = ((props) => {
   return (
     <div className='logo'>
-      <img src={props.logoPath} alt='logo' />
+      <img src={props.logoPath} width={props.width} alt='logo' />
     </div>
   );
 });
--- app/pages/Home.jsx
+++ app/pages/Home.jsx
@@ -5,10 +5,10 @@ class Home extends React.Component {
   render() {
     return (
       <div className="landing">
-        <h1 className="landing__title"> React electron ring client POC</h1>
         <div className="App-inner">
-          <Logo logoPath='../public/images/logo-ring-beta2-tagline-EN-couleur.svg' />
-          {/* <Logo logoPath='../public/images/logo-ring.svg' /> */}
+          <Logo
+            logoPath='../public/images/logo-ring-beta2-tagline-EN-couleur.svg'
+             width="350" />
           <h1 className="landing__title"> React electron ring client POC</h1>
         </div>
       </div>
